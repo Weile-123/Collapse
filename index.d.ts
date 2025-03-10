6c994e1bd9bbb554fb3dd34989a2920351bf4fa8
Huangweile <1294004813@qq.com>
2025-03-10 15:25:49 +0800

    index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare module 'weile-collapse' {
+    import { ComponentType } from 'react';
+
+    interface DataItem {
+        title: string;
+        content: string;
+    }
+
+    export interface CollapseProps {
+        data: DataItem[];
+        bgColor?: string;
+        titleColor?: string;
+        contentColor?: string;
+        img?: string;
+        speed?: number;
+        checked?: number | null;
+        titleFontSize?: number;
+        contentFontSize?: number;
+        onChange: (index: number) => void;
+    }
+
+    const Collapse: ComponentType<CollapseProps>;
+
+    export default Collapse;
+}
